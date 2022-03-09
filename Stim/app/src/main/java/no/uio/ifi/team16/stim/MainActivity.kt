2cd5bdd67913fa55748daaade92a5585ef7e3773
Filip <filipnh@uio.no>
2022-03-09 15:18:22 +0100

    Tom app

---
+++ Stim/app/src/main/java/no/uio/ifi/team16/stim/MainActivity.kt
@@ -0,0 +1,11 @@
+package no.uio.ifi.team16.stim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
