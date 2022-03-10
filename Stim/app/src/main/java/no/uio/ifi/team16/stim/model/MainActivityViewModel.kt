ad2f794f82f33277d443805a09e56a6fdf48069b
christian <christiansveen@hotmail.com>
2022-03-10 22:29:43 +0100

    InfectiousPressureDataloader now properly loads data.

---
+++ Stim/app/src/main/java/no/uio/ifi/team16/stim/model/MainActivityViewModel.kt
@@ -0,0 +1,29 @@
+package no.uio.ifi.team16.stim.model
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import no.uio.ifi.team16.stim.data.InfectiousPressure
+import no.uio.ifi.team16.stim.data.InfectiousPressureRepository
+
+class MainActivityViewModel : ViewModel() {
+    private val TAG = "MainActivityViewModel"
+    private val infectiousPressureRepository = InfectiousPressureRepository()
+    private val infectiousPressureData  = MutableLiveData<InfectiousPressure>()
+
+    fun getInfectiousPressureData() : MutableLiveData<InfectiousPressure> { return infectiousPressureData }
+
+    fun loadInfectiousPressure() {
+        viewModelScope.launch(Dispatchers.IO) {
+            Log.d(TAG, "loading infectiousdata to viewmodel")
+            val loaded = infectiousPressureRepository.getData() //either loaded, retrieved from cache or faked
+            Log.d("MADE", loaded.toString() )
+            Log.d(TAG, "loading infectiousdata to viewmodel - DONE")
+            //TODO: do not set here!
+            //infectiousPressureData.value = loaded //invokes observer
+        }
+    }
+}
