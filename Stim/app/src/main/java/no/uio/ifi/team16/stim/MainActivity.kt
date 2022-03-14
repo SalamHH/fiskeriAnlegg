f457c7caa04b18a9170d38dba7f53a41068eb2b6
Selma Margrethe Klouman Isaksson <smisakss@ulrik.uio.no>
2022-03-14 16:31:42 +0100

    Fungerende (?) Recycleview lagt til
    - tar inn infectiousPressure
    
    Må utvikles videre da men d er en start ;-)

--- Stim/app/src/main/java/no/uio/ifi/team16/stim/MainActivity.kt
+++ Stim/app/src/main/java/no/uio/ifi/team16/stim/MainActivity.kt
@@ -5,10 +5,9 @@ import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import no.uio.ifi.team16.stim.data.Site
-import no.uio.ifi.team16.stim.data.Sites
+import no.uio.ifi.team16.stim.model.viewModel.MainActivityViewModel
+import no.uio.ifi.team16.stim.model.viewModel.RecycleViewAdapter
 import no.uio.ifi.team16.stim.io.viewModel.MainActivityViewModel
-import no.uio.ifi.team16.stim.io.viewModel.RecycleViewAdapter
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
@@ -18,27 +17,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         /********************
-         *  RECYCLEVIEW *
+         * TEST RECYCLEVIEW *
          ********************/
 
         setContentView(R.layout.recycleview)
         val recycleview = findViewById<RecyclerView>(R.id.recyclerview)
-
-        /**
-         *  Tester Recycleview med dummy data
-         */
-        val dummySite1 = Site(0, "TUHOLMANE Ø", 59.371233, 5.216333)
-        val dummySite2 = Site(1, "TJAJNELUOKTA", 67.892433, 16.236718)
-        val dummySite3 = Site(2, "JØRSTADSKJERA", 59.2955, 5.938617)
-        val dummySite4 = Site(2, "ORHOLMBUKTA", 58.66277, 5.938221)
-        val dummySite5 = Site(2, "DIESELSTAD", 59.1134, 5.9383444)
-
-        val dummyList: List<Site> =
-            listOf(dummySite1, dummySite2, dummySite3, dummySite4, dummySite5)
-        val dummySites = Sites(dummyList)
-
-        //Loading adapter med Dummydata
-        recycleview.adapter = RecycleViewAdapter(dummySites)
+        recycleview.adapter = RecycleViewAdapter(null)
 
         /*************
          * OBSERVERS *
@@ -49,6 +33,12 @@ class MainActivity : AppCompatActivity() {
             println("THE INFECTIOUS PRESSURE OBSERVED IS \n" + infectiousPressure.toString())
 
             //do something with the infectious pressure data.
+
+            //RECYCLEVIEWTEST
+            Log.d("RECYCLEVIEW_TEST :", "Trying to add adapter")
+            recycleview.adapter = RecycleViewAdapter(infectiousPressure)
+            Log.d("RECYCLEVIEW_TEST: ", "Adapter added")
+
         }
 
         //observe sites
