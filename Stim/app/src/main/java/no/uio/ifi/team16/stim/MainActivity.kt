ef6b85482a90582d12ff90ac53494027aa766a39
christian <christiansveen@hotmail.com>
2022-04-01 15:51:01 +0200

    fix non-nullable in infectionfragment

--- Stim/app/src/main/java/no/uio/ifi/team16/stim/MainActivity.kt
+++ Stim/app/src/main/java/no/uio/ifi/team16/stim/MainActivity.kt
@@ -1,8 +1,7 @@
 package no.uio.ifi.team16.stim
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.WindowManager
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -28,69 +27,19 @@ class MainActivity : StimActivity() {
 
         // Navigation control for fragments
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
 
         //initial load of data
         viewModel.loadNorKyst800()
         viewModel.loadInfectiousPressure()
         viewModel.loadSites(Options.fakeMunicipality)
 
-        /**
-         * DISSE METODENE TRENGS IKKE FOR RECYCLEVIEW MEN KANSKJE NOE ANNET??
-         */
-        /*
-        //observe infectious pressure
-        viewModel.getInfectiousPressureData().observe(this) { infectiousPressure ->
-            Log.d("INVOKED", "observer of infectiousPressure")
-            println("THE INFECTIOUS PRESSURE OBSERVED IS \n" + infectiousPressure.toString())
-            //set adapter of recyclerview
-            recycleview.adapter = RecycleViewAdapter(
-                this,
-                viewModel.getSitesData().value ?: Sites(listOf()),
-                infectiousPressure,
-                viewModel.getNorKyst800Data().value
-            )
-
-            //Log.d(TAG, infectiousPressure?.getLatitude(4,4).toString())
-            Log.d(TAG, infectiousPressure?.getLatitude(4, 4).toString())
-            Log.d(TAG, infectiousPressure?.getLongitude(4, 4).toString())
-            val lat = infectiousPressure!!.getLatitude(4, 4)
-            val lng = infectiousPressure!!.getLongitude(4, 4)
-            Log.d(TAG, infectiousPressure?.getEtaRho(4).toString())
-            Log.d(TAG, infectiousPressure?.getXiRho(4).toString())
-            Log.d(TAG, infectiousPressure.project(lat, lng).toString())
-            val crsFactory = CRSFactory()
-            val stereographicProjection = crsFactory.createFromParameters(
-                null,
-                "+proj=stere +ellps=WGS84 +lat_0=90.0 +lat_ts=60.0 +x_0=3192800 +y_0=1784000 +lon_0=70"
-            )
-            val latLngCRT = stereographicProjection.createGeographic()
-            val ctFactory = CoordinateTransformFactory()
-            val latLngToStereo: CoordinateTransform =
-                ctFactory.createTransform(latLngCRT, stereographicProjection)
-            // `result` is an output parameter to `transform()`
-            // `result` is an output parameter to `transform()`
-            val result = ProjCoordinate()
-            latLngToStereo.transform(ProjCoordinate(lng.toDouble(), lat.toDouble()), result)
-            Log.d(TAG, "\n\n\n" + result.toString() + "\n\n\n")
-        }
-
-
-
         //observe norKyst800
-        viewModel.getNorKyst800Data().observe(this) { norKyst800 ->
-            Log.d("INVOKED", "observer of norkystPressure")
-            println("THE NORKYST800 OBSERVED IS \n" + norKyst800.toString())
-            //set adapter of recyclerview
-            recycleview.adapter = RecycleViewAdapter(
-                this,
-                viewModel.getSitesData().value ?: Sites(listOf()),
-                viewModel.getInfectiousPressureData().value,
-                norKyst800
-            )
+        viewModel.getInfectiousPressureTimeSeriesData().observe(this) { inf ->
+            Log.d(TAG, "observed change in infectiousPressureTimeseries")
         }
 
-         */
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
