97393f7b59b9f9604ee0b7ff2fb73ba890f10e84
Filip <filipnh@uio.no>
2022-04-12 12:36:14 +0200

    Temp fix for name cache

--- Stim/app/src/main/java/no/uio/ifi/team16/stim/data/repository/SitesRepository.kt
+++ Stim/app/src/main/java/no/uio/ifi/team16/stim/data/repository/SitesRepository.kt
@@ -2,7 +2,6 @@ package no.uio.ifi.team16.stim.data.repository
 
 import no.uio.ifi.team16.stim.data.Sites
 import no.uio.ifi.team16.stim.data.dataLoader.SitesDataLoader
-import android.util.Log
 
 /**
  * Repository for sites.
@@ -13,47 +12,48 @@ class SitesRepository {
     private val TAG = "SitesRepository"
     private val dataSource = SitesDataLoader()
 
-    //cache maps a municipalitynr to a list of sites
-    var cache: MutableMap<String, Sites?> = mutableMapOf()
+    /**
+     * Maps municipality-code to site list
+     */
+    private val municipalityCache: MutableMap<String, Sites?> = mutableMapOf()
 
-    //some sites, prefferably we will use the above cache, but not all municipalities have Sites
-    var someData: Sites? = null
+    /**
+     * Maps sitename to site object
+     * TODO: Fix this to be a Map<String, Site>
+     */
+    private val nameCache: MutableMap<String, Sites?> = mutableMapOf()
 
     /**
      * load the sites at the given municipalitycode
      */
     suspend fun getData(municipalityCode: String): Sites? {
-        var sites = cache[municipalityCode]
+        var sites = municipalityCache[municipalityCode]
         if (sites != null) {
             return sites
         }
         sites = dataSource.loadDataByMunicipalityCode(municipalityCode)
-        cache[municipalityCode] = sites
+
+        if (sites != null) {
+            municipalityCache[municipalityCode] = sites
+            for (site in sites.sites) {
+                // todo this makes no sense
+                nameCache[site.name] = sites
+            }
+        }
+
         return sites
     }
 
+    /**
+     * Load the site with the given name
+     */
     suspend fun getDataByName(name: String): Sites? {
-        var sites = cache[name]
+        var sites = nameCache[name]
         if (sites != null) {
             return sites
         }
         sites = dataSource.loadDataByName(name)
-        cache[name] = sites
+        nameCache[name] = sites
         return sites
     }
-
-
-
-
-    /**
-     * Load some data, the 100 first sites.
-     * used initially to just get some placeholder data,
-     * does not properly cache
-     */
-    /*suspend fun getSomeData(): Sites? {
-        Log.d(TAG, "loading 100 first sitesdata from repository")
-        someData = dataSource.loadSomeData()
-        Log.d(TAG, "loading 100 first sitesdata from repository - DONE")
-        return someData
-    }*/
 }
