d0c46036beb771610a63f272c142cafc39b88960
Selma Margrethe Klouman Isaksson <smisakss@ulrik.uio.no>
2022-05-10 12:42:09 +0200

    extra decimals temperatur

--- Stim/app/src/main/java/no/uio/ifi/team16/stim/TempValueFormatter.kt
+++ Stim/app/src/main/java/no/uio/ifi/team16/stim/TempValueFormatter.kt
@@ -7,12 +7,12 @@ import com.github.mikephil.charting.formatter.ValueFormatter
 class TempValueFormatter : ValueFormatter() {
 
     override fun getAxisLabel(value: Float, axis: AxisBase?): String {
-        return String.format("%.0f°", value)
+        return String.format("%.2f°", value)
     }
 
     // override this for e.g. LineChart or ScatterChart
     override fun getPointLabel(entry: Entry?): String {
-        return String.format("%.0f°", entry?.y)
+        return String.format("%.2f°", entry?.y)
     }
 
 }
