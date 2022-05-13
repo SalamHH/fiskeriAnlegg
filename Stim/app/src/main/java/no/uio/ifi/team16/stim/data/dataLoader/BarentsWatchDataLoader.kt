19100cd8a7fbdd1531901b9a1c4dee781e326f75
Christian <casveen@student.matnat.uio.no>
2022-05-13 13:59:03 +0200

    suspending await and properly failing access

--- Stim/app/src/main/java/no/uio/ifi/team16/stim/data/dataLoader/BarentsWatchDataLoader.kt
+++ Stim/app/src/main/java/no/uio/ifi/team16/stim/data/dataLoader/BarentsWatchDataLoader.kt
@@ -4,12 +4,13 @@ import android.util.Log
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.core.Headers
 import com.github.kittinunf.fuel.core.extensions.authentication
-import com.github.kittinunf.fuel.coroutines.awaitString
+import com.github.kittinunf.fuel.coroutines.awaitStringResult
+import com.github.kittinunf.result.getOrElse
+import com.github.kittinunf.result.onError
 import no.uio.ifi.team16.stim.data.BarentsWatchAtSite
 import no.uio.ifi.team16.stim.data.BarentsWatchToken
 import no.uio.ifi.team16.stim.data.Site
 import org.json.JSONObject
-import java.io.UnsupportedEncodingException
 import java.net.URLEncoder
 import java.time.Instant
 
@@ -18,7 +19,6 @@ class BarentsWatchDataLoader {
 
     /**
      * Retrieves an authentication token to be used in API requests to the BW API
-     * TODO: should use suspending post, this one <seems> to block
      */
     suspend fun getToken(): BarentsWatchToken? {
 
@@ -33,34 +33,46 @@ class BarentsWatchDataLoader {
 
         val (request, response, result) = Fuel.post("https://id.barentswatch.no/connect/token")
             .header(Headers.CONTENT_TYPE, "application/x-www-form-urlencoded")
-                //.header(Headers.CONTENT_LENGTH, "344")
-                //.header("Host", "www.barentswatch.no")
-                .header(Headers.USER_AGENT, "PostmanRuntime/7.29.0")
+            .header(Headers.USER_AGENT, "PostmanRuntime/7.29.0")
             .header(Headers.ACCEPT, "*/*")
             .header(Headers.ACCEPT_ENCODING, "gzip, deflate, br")
             .header("Connection", "keep-alive")
             .body(body)
             .responseString()
 
+        //await result and handle error if any
+        val tokenStr: String = request.awaitStringResult().onError { error ->
+            Log.e(TAG, "Failed to post to BarentsWatch API due to:\n $error")
+            return null
+        }.getOrElse { err ->
+            Log.e(
+                TAG,
+                "Failed to get response from BarentsWatch API due to$err"
+            )
+            return null
+        }
+
         Log.d(TAG, request.toString())
         Log.d(TAG, response.toString())
         Log.d(TAG, result.toString())
 
-        //TODO: return null if failure
-
-        val tokenStr = result.component1()
-        Log.d(TAG, tokenStr.toString())
+        Log.d(TAG, tokenStr)
         val jsonObject = JSONObject(tokenStr)
-        val token = jsonObject.getString("access_token")
-        Log.d(TAG, token)
-
-        val validityTime = jsonObject.getString("expires_in").toLong()
-
+        val validityTime: Long
+        val token: String
+        try {
+            token = jsonObject.getString("access_token")
+            validityTime = jsonObject.getString("expires_in").toLong()
+            Log.d(TAG, token)
+        } catch (e: Error) {
+            Log.e(TAG, "Failed to read token from response due to $e")
+            return null
+        }
         return BarentsWatchToken(token, Instant.now().plusSeconds(validityTime))
     }
 
-    @Throws(UnsupportedEncodingException::class)
-    private fun getDataString(params: HashMap<String, String>): String {
+    //@Throws(UnsupportedEncodingException::class)
+    private suspend fun getDataString(params: HashMap<String, String>): String {
         val result = StringBuilder()
         var first = true
         for ((key, value) in params) {
@@ -72,70 +84,104 @@ class BarentsWatchDataLoader {
         return result.toString()
     }
 
-    public suspend fun loadData(site: Site, token: BarentsWatchToken): BarentsWatchAtSite? {
+    suspend fun loadData(site: Site, token: BarentsWatchToken): BarentsWatchAtSite? {
 
-        val listPD = getPdList(site, token)
-        val listILA = getIlaList(site, token)
+        val listPD = getPdList(site, token) ?: return null
+        val listILA = getIlaList(site, token) ?: return null
 
-        //forhåpentligvis kan attributter i site brukes for å gjøre requesten.
-        //TODO null if failure
         return BarentsWatchAtSite(listPD, listILA)
     }
 
-    private suspend fun getIlaList(site: Site, token: BarentsWatchToken): java.util.HashMap<String, String> {
+    private suspend fun getIlaList(site: Site, token: BarentsWatchToken): HashMap<String, String>? {
         val listILA = HashMap<String, String>()
         val baseUrlILA = "https://www.barentswatch.no/bwapi/v1/geodata/download/localitieswithila"
         val lon = site.latLong.lng
         val lat = site.latLong.lat
         val distance = 1.0
 
-        val responseStr = Fuel.get(baseUrlILA, listOf("format" to "JSON", "lon" to lon, "lat" to lat, "distance" to distance))
+        val responseStr = Fuel.get(
+            baseUrlILA,
+            listOf("format" to "JSON", "lon" to lon, "lat" to lat, "distance" to distance)
+        )
             .authentication()
             .bearer(token.key)
-            .awaitString()
+            .awaitStringResult()
+            .onError { error ->
+                Log.e(TAG, "Failed to read ILA list from BarentsWatch API due to:\n $error")
+                return null
+            }.getOrElse { err ->
+                Log.e(
+                    TAG,
+                    "Failed to get ILA list from from BarentsWatch API due to $err"
+                )
+                return null
+            }
 
         val response = JSONObject(responseStr)
-        val totalFeatures = response.getString("totalFeatures")
-        if (totalFeatures == "0") {
-            return HashMap<String, String>()
-        }
+        try {
+            val totalFeatures = response.getString("totalFeatures")
+            if (totalFeatures == "0") {
+                return HashMap()
+            }
 
-        val features = response.getJSONArray("features")
-        val jsonObject = features.getJSONObject(0)
-        val properties = jsonObject.getJSONObject("properties")
-        listILA["mistankedato"] = properties.getString("mistankedato")
-        listILA["paavistdato"] = properties.getString("paavistdato")
+            val features = response.getJSONArray("features")
+            val jsonObject = features.getJSONObject(0)
+            val properties = jsonObject.getJSONObject("properties")
+            listILA["mistankedato"] = properties.getString("mistankedato")
+            listILA["paavistdato"] = properties.getString("paavistdato")
+        } catch (e: Error) {
+            Log.e(TAG, "Failed to parse ILA from response due to $e")
+            return null
+        }
 
         return listILA
     }
 
-    private suspend fun getPdList(site: Site, token: BarentsWatchToken): HashMap<String, String> {
+    private suspend fun getPdList(site: Site, token: BarentsWatchToken): HashMap<String, String>? {
         val listPD = HashMap<String, String>()
         val baseUrlPD = "https://www.barentswatch.no/bwapi/v1/geodata/download/localitieswithpd"
         val lon = site.latLong.lng
         val lat = site.latLong.lat
         val distance = 1.0
 
-        val responseStr = Fuel.get(baseUrlPD, listOf("format" to "JSON", "lon" to lon, "lat" to lat, "distance" to distance))
+        val responseStr = Fuel.get(
+            baseUrlPD,
+            listOf("format" to "JSON", "lon" to lon, "lat" to lat, "distance" to distance)
+        )
             .authentication()
             .bearer(token.key)
-            .awaitString()
+            .awaitStringResult()
+            .onError { error ->
+                Log.e(TAG, "Failed to read PD list from BarentsWatch API due to:\n $error")
+                return null
+            }.getOrElse { err ->
+                Log.e(
+                    TAG,
+                    "Failed to get PD list from from BarentsWatch API due to $err"
+                )
+                return null
+            }
 
         val response = JSONObject(responseStr)
-        val totalFeatures = response.getString("totalFeatures")
-        if (totalFeatures == "0") {
-            return HashMap<String, String>()
-        }
+        try {
+            val totalFeatures = response.getString("totalFeatures")
+            if (totalFeatures == "0") {
+                return HashMap()
+            }
 
-        val features = response.getJSONArray("features")
-        val jsonObject = features.getJSONObject(0)
-        val properties = jsonObject.getJSONObject("properties")
-        listPD["pd_ukjent_mistankedato"] = properties.getString("pd_ukjent_mistankedato")
-        listPD["pd_sav2_mistankedato"] = properties.getString("pd_sav2_mistankedato")
-        listPD["pd_sav3_mistankedato"] = properties.getString("pd_sav3_mistankedato")
-        listPD["pd_ukjent_paavistdato"] = properties.getString("pd_ukjent_paavistdato")
-        listPD["pd_sav2_paavistdato"] = properties.getString("pd_sav2_paavistdato")
-        listPD["pd_sav3_paavistdato"] = properties.getString("pd_sav3_paavistdato")
+            val features = response.getJSONArray("features")
+            val jsonObject = features.getJSONObject(0)
+            val properties = jsonObject.getJSONObject("properties")
+            listPD["pd_ukjent_mistankedato"] = properties.getString("pd_ukjent_mistankedato")
+            listPD["pd_sav2_mistankedato"] = properties.getString("pd_sav2_mistankedato")
+            listPD["pd_sav3_mistankedato"] = properties.getString("pd_sav3_mistankedato")
+            listPD["pd_ukjent_paavistdato"] = properties.getString("pd_ukjent_paavistdato")
+            listPD["pd_sav2_paavistdato"] = properties.getString("pd_sav2_paavistdato")
+            listPD["pd_sav3_paavistdato"] = properties.getString("pd_sav3_paavistdato")
+        } catch (e: Error) {
+            Log.e(TAG, "Failed to parse PD from response due to $e")
+            return null
+        }
 
         return listPD
     }
